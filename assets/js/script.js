81f05f5768cdf7e89e1374fe8f365a17eaea7ba6
Tom Cowen <tomreescowen@gmail.com>
2024-01-30 13:12:23 +0000

    Add divion game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,7 +147,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
     
-    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand1').textContent = parseInt(operand1 * operand2);
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "÷";
     
